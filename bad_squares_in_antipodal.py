f6cec87698817c728d92f371839800037ea2cc83
Alica Domanyova <11502862@cuni.cz>
2025-12-11 09:38:18 +0100

    Might be working

--- bad_squares_in_antipodal.py
+++ bad_squares_in_antipodal.py
@@ -3,6 +3,8 @@ from argparse import ArgumentParser
 
 
 DIMENSION = int(input())
+NUM_OF_VERTICES = 1 << DIMENSION
+NUM_OF_EDGES = NUM_OF_VERTICES * DIMENSION / 2
 
 def call_solver(cnf, nr_vars, output_name, solver_name, verbosity):
     # print CNF into formula.cnf in DIMACS format
@@ -19,44 +21,110 @@ def E(u, v):
         u,v = v,u
     if (u ^ v).bit_count() != 1:
         raise Exception("vertices", u, ",", v, "do not share an edge")
-    e = (u << DIMENSION) + v - u - 1
-MAX_E = E((1 << DIMENSION) - 2, (1 << DIMENSION) - 1)
+    e = (u << DIMENSION) + v - u
+    return int(e)
+MAX_E = E(NUM_OF_VERTICES - 2, NUM_OF_VERTICES - 1)
 
-def R(v):
-    return MAX_E + v + 1
-MAX_R = R((1 << DIMENSION) - 1)
-
-def H(u, v):
+def H(u, v, color, start):
     if (u ^ v).bit_count() != 1:
         raise Exception("vertices", u, ",", v, "do not share an edge")
+    if (v == start):
+        raise Exception("edge", u, ",", v, "goes to the start vertex")
     h = (u << DIMENSION) + v - u
-    if v > u: h -= 1
-    return MAX_R + h
-MAX_H = H((1 << DIMENSION) - 2, (1 << DIMENSION) - 1)
+
+
+    if color == "red" and start == 0:
+        return int(MAX_E + h)
+    elif color == "blue" and start == 0:
+        return int(MAX_E + NUM_OF_EDGES + h)
+    elif color == "red" and start == 1:
+        return int(MAX_E + 2*NUM_OF_EDGES + h)
+    elif color == "blue" and start == 1:
+        return int(MAX_E + 3*NUM_OF_EDGES + h)
+    else: raise Exception("the input value of color or start is wrong")
+MAX_H = H(NUM_OF_VERTICES - 1, NUM_OF_VERTICES - 2, "blue", 1)
+
+# is vertex v reachable from 0 or 1 only using blue or red edges?
+def V(v, color, start):
+    if color == "red" and start == 0:
+        return int(MAX_H + v + 1)
+    elif color == "blue" and start == 0:
+        return int(MAX_H + NUM_OF_VERTICES + v + 1)
+    elif color == "red" and start == 1:
+        return int(MAX_H + 2*NUM_OF_VERTICES + v + 1)
+    elif color == "blue" and start == 1:
+        return int(MAX_H + 3*NUM_OF_VERTICES + v + 1)
+    else: raise Exception("the input value of color or start is wrong")
+MAX_V = V(NUM_OF_VERTICES - 1, "blue", 1)
+
+def anti(u):
+    v = 0
+    for i in range(DIMENSION):
+        v += (1 ^ ((u >> i) - ((u >> (i+1)) << 1))) << i
+    return int(v)
 
 def antipodalLogic():
-    ...
+    logic = []
+    for u in range(NUM_OF_VERTICES):
+        for d in range(DIMENSION):
+            v = u ^ (1 << d)
+            logic.append([E(u,v), E(anti(u), anti(v))])
+            logic.append([- E(u,v), - E(anti(u), anti(v))])
+    return logic
+
 
 def pathLogic():
-    ...
+    logic = []
+    for color in ["red", "blue"]:
+        for start in [0, 1]:
+            logic.append([V(start, color, start)])
+            # TODO vyriesit H sem... ci netreba?
+            logic.append([ - V(start ^ 3, color, start)])
+            c = (-1)**(color == "blue")
+            for u in range(NUM_OF_VERTICES):
+                for d in range(DIMENSION):
+                    v = u ^ (1 << d)
+                    if (v == start):
+                        continue
+                    logic.append([ - H(u, v, color, start), V(u, color, start)])
+                    logic.append([ - H(u, v, color, start), E(u, v) * c])
+                    logic.append([H(u, v, color, start), - V(u, color, start), -E(u,v) * c])
+
+            for v in range(NUM_OF_VERTICES):
+                if (v == start):
+                        continue
+                big_clause = [-V(v, color, start)]
+                for d in range(DIMENSION):
+                    u = v ^ (1 << d)
+                    logic.append([V(v, color, start), - H(u, v, color, start)])
+                    big_clause.append(H(u, v, color, start))
+                logic.append(big_clause)
+
+    return logic
+    
+
 
 def getTrueColors(result):
-    ...
+    model = []
+    for line in result.stdout.decode('utf-8').split('\n'):
+        if line.startswith("v"):    # there might be more lines of the model, each starting with 'v'
+            vars = line.split(" ")
+            vars.remove("v")
+            model.extend(int(v) for v in vars)      
+    #model.remove(0) # 0 is the end of the model, just ignore it
+    red = []
+    for m in model:
+        if (abs(m) <= MAX_E and m > 0): 
+            u = m >> DIMENSION
+            v = (m - (u << DIMENSION)) + u
+            red.append([u, v])
+    return red
 
 
 if __name__ == "__main__":
 
     parser = ArgumentParser()
 
-    parser.add_argument(
-        "-i",
-        "--input",
-        default="input.in",
-        type=str,
-        help=(
-            "The instance file."
-        ),
-    )
     parser.add_argument(
         "-o",
         "--output",
@@ -92,7 +160,9 @@ if __name__ == "__main__":
     cnf = []
     cnf.extend(antipodalLogic())
     cnf.extend(pathLogic())
-    nr_vars = MAX_H
+    nr_vars = MAX_V
+
+    #print(cnf)
 
     # now we know the max clique has size smallEnd
     # we run the  SAT solver one last time to get the vertices of the clique
